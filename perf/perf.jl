420eb39378219771a3e16b71c38975e9a4f3662f
Guido Kraemer <gdkrmr@users.noreply.github.com>
2018-02-13 21:28:02 +0100

    make benchmarks print again

--- perf/perf.jl
+++ perf/perf.jl
@@ -4,11 +4,11 @@ using BenchmarkTools
 
 reload("Ripemd")
 d = b"";
-@benchmark Ripemd.ripemd160($d);
+@benchmark Ripemd.ripemd160($d)
 @benchmark Nettle.digest("ripemd160", $d)
 
 d = b"a";
-@benchmark Ripemd.ripemd160($d);
+@benchmark Ripemd.ripemd160($d)
 @benchmark Nettle.digest("ripemd160", $d)
 
 d = [b"a"[1] for j in 1:10000];
