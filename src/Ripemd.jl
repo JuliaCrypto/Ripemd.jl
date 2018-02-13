c0bf541ecaf7cd5efddddb0b655e3f75c3392f8b
Guido Kraemer <gdkrmr@users.noreply.github.com>
2018-02-13 20:29:06 +0100

    formatting

--- src/Ripemd.jl
+++ src/Ripemd.jl
@@ -9,7 +9,8 @@ include("types.jl")
 include("interface.jl")
 include("transform.jl")
 
-function ripemd160(data::T) where T<:Union{Array{UInt8,1},NTuple{N,UInt8} where N}
+function ripemd160(data::T) where T <: Union{Array{UInt8, 1},
+                                             NTuple{N, UInt8} where N}
     ctx = RIPEMD160_CTX()
     update!(ctx, data)
     return digest!(ctx)
