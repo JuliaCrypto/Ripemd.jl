7b35d246b32d3a5acedbd04cd6ae8b77f6615000
Guido Kraemer <gdkrmr@users.noreply.github.com>
2018-05-27 15:25:58 +0200

    more rollback

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ files = readdir(".")
 
 # use only files that are named test_*.jl and start with the newest
 
-filter!(x -> occursin(r"^test_", x), files)
+filter!(x -> contains(x, r"^test_"), files)
 sort!(files, by = x -> stat(x).mtime, rev = true)
 
 for f in files
