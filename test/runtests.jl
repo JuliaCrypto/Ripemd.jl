285387fa499b31cbb10267fdff2ce9e4e8d15226
Guido Kraemer <gdkrmr@users.noreply.github.com>
2018-05-27 15:15:16 +0200

    fix 0.7 warnings

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using Ripemd
-using Base.Test
+using Test
 
 files = readdir(".")
 
 # use only files that are named test_*.jl and start with the newest
 
-filter!(x -> contains(x, r"^test_"), files)
+filter!(x -> occursin(r"^test_", x), files)
 sort!(files, by = x -> stat(x).mtime, rev = true)
 
 for f in files
