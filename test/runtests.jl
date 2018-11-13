99141c8a3999c16081f78edabb8c59de1ba1cf0b
gdkrmr <gdkrmr@users.noreply.github.com>
2018-11-13 17:32:56 +0100

    version compatibility julia 0.6 -- 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,20 @@
 using Ripemd
-using Base.Test
+if VERSION < v"1.0.0"
+    using Base.Test
+else
+    using Test
+end
 
 files = readdir(".")
 
 # use only files that are named test_*.jl and start with the newest
 
-filter!(x -> contains(x, r"^test_"), files)
+if VERSION < v"0.7.0"
+    filter!(x -> contains(x, r"^test_"), files)
+else
+    filter!(x -> occursin(r"^test_", x), files)
+end
+
 sort!(files, by = x -> stat(x).mtime, rev = true)
 
 for f in files
