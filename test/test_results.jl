66dcfd5a8a69186ddd10f7cb5c3beea11dc2eaa0
Bill <wherrera10@users.noreply.github.com>
2026-09-08 20:19:58 -1000

    Remove duplicate ROCKY_SPEECH and ROCKY_RIPEMD160

--- test/test_results.jl
+++ test/test_results.jl
@@ -7,6 +7,9 @@
 
 const LIBCRYPTO = "libcrypto"
 
+const ROCKY_SPEECH = "I'd hold you up to say to your mother, 'this kid's gonna be the best kid in the world. This kid's gonna be somebody better than anybody I ever knew.' And you grew up good and wonderful. It was great just watching you, every day was like a privilege. Then the time come for you to be your own man and take on the world, and you did. But somewhere along the line, you changed. You stopped being you. You let people stick a finger in your face and tell you you're no good. And when things got hard, you started looking for something to blame, like a big shadow. Let me tell you something you already know. The world ain't all sunshine and rainbows. It's a very mean and nasty place and I don't care how tough you are it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain't about how hard ya hit. It's about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That's how winning is done! Now if you know what you're worth then go out and get what you're worth. But ya gotta be willing to take the hits, and not pointing fingers saying you ain't where you wanna be because of him, or her, or anybody! Cowards do that and that ain't you! You're better than that! I'm always gonna love you no matter what. No matter what happens. You're my son and you're my blood. You're the best thing in my life. But until you start believing in yourself, ya ain't gonna have a life. Don't forget to visit your mother."
+const ROCKY_RIPEMD160 = "fff55c23c197b4fded67e09424e5aef9dafad1c6"
+
 function openssl_ripemd160_bytes(data::Vector{UInt8})
     md = ccall((:EVP_get_digestbyname, LIBCRYPTO), Ptr{Cvoid}, (Cstring,), "RIPEMD160")
     md == C_NULL && error("RIPEMD160 not available in this OpenSSL build " *
@@ -91,9 +94,6 @@ end
         "5d0689ef49d2fae572b881b123a85ffa21595f36"
     @test bytes2hex(Ripemd.ripemd160("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")) ==
         "b0e20b6e3116640286ed3a87a5713079b21f5189"
-
-    const ROCKY_SPEECH = "I'd hold you up to say to your mother, 'this kid's gonna be the best kid in the world. This kid's gonna be somebody better than anybody I ever knew.' And you grew up good and wonderful. It was great just watching you, every day was like a privilege. Then the time come for you to be your own man and take on the world, and you did. But somewhere along the line, you changed. You stopped being you. You let people stick a finger in your face and tell you you're no good. And when things got hard, you started looking for something to blame, like a big shadow. Let me tell you something you already know. The world ain't all sunshine and rainbows. It's a very mean and nasty place and I don't care how tough you are it will beat you to your knees and keep you there permanently if you let it. You, me, or nobody is gonna hit as hard as life. But it ain't about how hard ya hit. It's about how hard you can get hit and keep moving forward. How much you can take and keep moving forward. That's how winning is done! Now if you know what you're worth then go out and get what you're worth. But ya gotta be willing to take the hits, and not pointing fingers saying you ain't where you wanna be because of him, or her, or anybody! Cowards do that and that ain't you! You're better than that! I'm always gonna love you no matter what. No matter what happens. You're my son and you're my blood. You're the best thing in my life. But until you start believing in yourself, ya ain't gonna have a life. Don't forget to visit your mother."
-    const ROCKY_RIPEMD160 = "fff55c23c197b4fded67e09424e5aef9dafad1c6"
 
     @test bytes2hex(Ripemd.ripemd160(ROCKY_SPEECH)) == ROCKY_RIPEMD160
 
