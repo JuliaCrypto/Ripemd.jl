f31875315eb58f1563e16d12d49fedd252a176dd
Bill <wherrera10@users.noreply.github.com>
2026-09-11 09:05:33 -1000

    Comment RIPEMD160_CTX struct and its properties
    Add documentation for Documenter. No actual code changes.

--- src/types.jl
+++ src/types.jl
@@ -1,6 +1,13 @@
 
+""" Abstract type for RIPEMD hash function contexts. Currently there is only RIPEMD160_CTX. """
 abstract type RIPEMD_CTX end
 
+"""
+    RIPEMD160_CTX
+
+Mutable struct representing the context for the RIPEMD160 hash function.
+Contains the internal `state`, the `count` of processed bytes, and a `buffer` for input data.
+"""
 mutable struct RIPEMD160_CTX <: RIPEMD_CTX
     state  :: Array{UInt32, 1} # length 5
     count  :: UInt64           # how many bytes we already ingested
@@ -8,13 +15,22 @@ mutable struct RIPEMD160_CTX <: RIPEMD_CTX
                                # transform!
 end
 
-# length of the buffer
+""" Length of the buffer for RIPEMD160_CTX blocks. """
 bytes_per_block(::Type{RIPEMD160_CTX}) = 64
+
+""" Number of 32-bit words in a RIPEMD160_CTX block. """
 words_per_block(::Type{RIPEMD160_CTX}) = 16
 
+""" Type of the internal state words for RIPEMD160. """
 state_type(::Type{RIPEMD160_CTX}) = UInt32
+
+""" Length of the final digest for RIPEMD160_CTX in bytes. """
 digest_length(::Type{RIPEMD160_CTX}) = 20
 
+""" Constructor for RIPEMD160_CTX.
+
+Returns a new instance of `RIPEMD160_CTX` with the initial state, zero count, and an empty buffer.
+"""
 function RIPEMD160_CTX()
     RIPEMD160_CTX(copy(INIT_STATE), 0 , zeros(UInt8, bytes_per_block(RIPEMD160_CTX)))
 end
