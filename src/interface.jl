a3f19aa94a10f8836896267d61d0335b595e78ce
Bill <wherrera10@users.noreply.github.com>
2026-09-11 16:15:45 -1000

    Enhance RIPEMD160 functions with documentation and fixes
    Updated the `update!` and `digest!` functions for RIPEMD160 to handle big-endian as well as little-endian architectures, include detailed documentation and widen argument handling.

--- src/interface.jl
+++ src/interface.jl
@@ -1,30 +1,52 @@
-function update!(ctx::T, data::U) where {T <: RIPEMD160_CTX,
-                                         U <: Union{DenseArray{UInt8, 1},
-                                                    NTuple{N, UInt8} where N}}
-    UIntXXX = typeof(ctx.count)
+"""
+    update!(ctx::RIPEMD160_CTX, data::Union{AbstractVector{UInt8}, NTuple{N,UInt8} where N})
 
-    len = convert(UIntXXX, length(data))
-    data_idx = convert(UIntXXX, 0)
-    usedspace = ctx.count % bytes_per_block(T)
+Update the RIPEMD160 context `ctx` with the given `data`. The data can be any packed UInt8 based array,
+eg. a vector of bytes, codeunits, bytes of a String, or a tuple of bytes.
 
-    while len - data_idx + usedspace >= bytes_per_block(T)
-        copyto!(ctx.buffer, usedspace + 1,
-                data,       data_idx + 1,
-                bytes_per_block(T))
+Arguments
+---------
+ctx : RIPEMD160_CTX
+    The RIPEMD160 context to update.
+data : Union{AbstractVector{UInt8}, NTuple{N,UInt8} where N}
+    The input data to update the context with.
+Returns
+-------
+Nothing. The context `ctx` is updated in place.
+"""
+function update!(ctx::RIPEMD160_CTX, data::Union{AbstractVector{UInt8}, NTuple{N,UInt8} where N})
+    len = length(data)
+    pos = 1
+
+    used = Int(ctx.count & 0x3f)
+
+    if used != 0
+        n = min(64 - used, len)
+        copyto!(ctx.buffer, used + 1, data, pos, n)
+        ctx.count += UInt64(n)
+        pos += n
+
+        if n < 64 - used
+            return nothing
+        end
 
         transform!(ctx)
-
-        ctx.count += bytes_per_block(T) - usedspace
-        data_idx += bytes_per_block(T) - usedspace
-        usedspace = convert(UIntXXX, 0)
+        used = 0
     end
 
-    if len > data_idx
-        copyto!(ctx.buffer, usedspace + 1,
-                data,       data_idx + 1,
-                len - data_idx)
-        ctx.count += len - data_idx
+    while pos + 63 <= len
+        copyto!(ctx.buffer, 1, data, pos, 64)
+        transform!(ctx)
+        ctx.count += UInt64(64)
+        pos += 64
     end
+
+    if pos <= len
+        n = len - pos + 1
+        copyto!(ctx.buffer, 1, data, pos, n)
+        ctx.count += UInt64(n)
+    end
+
     return nothing
 end
 
@@ -65,16 +87,33 @@ function pad_remainder!(ctx::T) where {T <: RIPEMD160_CTX}
     return nothing
 end
 
-function digest!(ctx::T) where {T <: RIPEMD160_CTX}
+"""
+    digest!(ctx::RIPEMD160_CTX)
+
+Finalize the RIPEMD160 hash computation and return the digest as a vector of bytes.
+Arguments
+---------
+ctx : RIPEMD160_CTX
+    The context containing the current state of the hash computation.
+Returns
+-------
+A vector of bytes representing the RIPEMD160 hash.
+"""
+function digest!(ctx::RIPEMD160_CTX)
     pad_remainder!(ctx)
 
-    # bitcount_idx = div(words_per_block(T), sizeof(ctx.count)) + 1
-    pbuf = Ptr{typeof(ctx.count)}(pointer(ctx.buffer))
-    unsafe_store!(pbuf, UInt64(ctx.count * 8), 8)
+    # RIPEMD-160 stores the 64-bit bit-length little-endian; `htol` makes
+    # this correct on big-endian hosts too (no-op on little-endian ones).
+    bits = htol(ctx.count << 3)
+
+    p = Ptr{UInt64}(pointer(ctx.buffer, 57))
+    unsafe_store!(p, bits)
 
     transform!(ctx)
 
-    # TODO: is this safe for 0.7? the [1:end] should make a copy and get around
-    # the lazy reinterpret.
-    return reinterpret(UInt8, ctx.state)[1:digest_length(T)]
+    # The five UInt32 state words must be emitted as little-endian bytes
+    # regardless of host order, so convert each word with `htol` before
+    # reinterpreting it as raw bytes.
+    le_state = map(htol, ctx.state)
+    reinterpret(UInt8, le_state)[1:20] # we need [1:20] to make a copy here
 end
